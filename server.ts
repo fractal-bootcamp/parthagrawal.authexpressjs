bbb5f9c562dbeacb0c55be7ff180b6b11dc2c4b8
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-10 10:40:10 -0400

    post request works

--- server.ts
+++ server.ts
@@ -1,12 +1,40 @@
+import bodyParser from "body-parser";
 import express from "express";
 // QUESTION - diff between above statement?
 // import { Express } from "express";
 
 const app = express()
-const port = 3000
+// what does this do? 
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+const port = 5001
+
+const users = [
+    {
+        id: '1',
+        email: 'parth@gmail.com',
+        password: 'password'
+    },
+    {
+        id: '2',
+        email: "jason@jasonmomoa.com",
+        password: 'baseball'
+    }
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/banana', (req, res) => {
+    console.log(req.body.user)
+    console.log(req.body.banana)
+})
+
+app.get('/login', (req, res) => {
+    res.send('<input type="text" />')
 })
 
 app.listen(port, () => {
